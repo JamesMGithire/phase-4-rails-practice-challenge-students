2c1734023be11b80626230b3db088f7ba47a18e4
James Mburu Githire <jamesmburug24@gmail.com>
2022-11-23 11:31:26 +0300

    added relationships, migrated & seeded

--- app/serializers/instructor_serializer.rb
+++ app/serializers/instructor_serializer.rb
@@ -1,3 +1,4 @@
 class InstructorSerializer < ActiveModel::Serializer
   attributes :id, :name
+  has_many :students
 end
--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,3 +1,4 @@
 class StudentSerializer < ActiveModel::Serializer
   attributes :id, :name, :major, :age
+  belongs_to :instructor
 end
