5bd0ca0068b88fb62faf68abcbf8c57fa21f6f36
James Mburu Githire <jamesmburug24@gmail.com>
2022-11-23 11:40:56 +0300

    added methods and error handlers

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,2 +1,40 @@
 class InstructorsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :invalid_record_method
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    def index
+        render json: Instructor.all
+    end
+
+    def show
+        instructor = find_instructor
+        render json: instructor
+    end
+
+    def create
+        instructor = Instructor.new(instructor_params)
+        render json: instructor, status: :created
+    end
+
+    def update
+        instructor = find_instructor
+        instructor.update(instructor_params)
+        render json: instructor
+    end
+
+    private
+    def find_instructor
+            instructor = Instructor.find_by(id: params[:id])
+    end
+
+    def instructor_params
+        params.permit(:name)
+    end
+
+    def record_not_found
+        render json: { error: 'Record not found' }, status: :not_found
+    end
+
+    def invalid_record_method e
+        render json: {errors: e.errors.messages}
+    end
 end
